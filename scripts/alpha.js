d3a516ec0f48e2d592c8c01150b768f776a804c8
Abdullalimon <mabdullahossin@gmail.com>
2024-02-14 16:47:43 +0600

    commit all done work

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -5,9 +5,17 @@
 //     const playGroundSection = document.getElementById('play-ground');
 //     playGroundSection.classList.remove('hidden');
 // }
+function continueGame(){
+  const alphabet = getRandomAlphabet();
+  console.log('your alphabets:',alphabet);
 
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+  setBackgroundColorById(alphabet);
+}
 
 function play(){
     hideElementById('start-page');
     showElementById('play-ground');
+    continueGame()
 }
