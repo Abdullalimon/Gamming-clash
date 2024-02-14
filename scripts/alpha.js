b9f11bf834bd7133934164f281fa350c0a533b3d
Abdullalimon <mabdullahossin@gmail.com>
2024-02-14 15:40:08 +0600

    added all the work

---
+++ scripts/alpha.js
@@ -0,0 +1,13 @@
+// function play(){
+//     const homeSection = document.getElementById('start-page');
+//     homeSection.classList.add('hidden');
+
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+// }
+
+
+function play(){
+    hideElementById('start-page');
+    showElementById('play-ground');
+}
